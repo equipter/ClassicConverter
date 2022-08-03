b30a143b0afd0e8cb9984ece8436eb2eb695190e
Alex <equipter@outlook.com>
2022-08-03 03:53:52 +0100

    added files

---
+++ ClassicConverter.py
@@ -0,0 +1,130 @@
+import argparse
+import os
+import pathlib
+from typing import Tuple
+# mifare classic 1k 4b uid binary to flipper .nfc converter. 
+# Created by Equipter with code portions borrowed from Lucaslhm/AmiiboFlipperConverter
+# V1.03
+
+
+def write_output(name: str, assemble: str, out_dir: str):
+    with open(os.path.join(out_dir, f"{name}.nfc"), "wt") as f:
+        f.write(assemble)
+
+
+def convert(contents: bytes) -> Tuple[str, int]:
+    buffer = []
+    Block_count = 0
+
+    Block = []
+    for i in range(len(contents) - 0):
+        byte = contents[i : i + 1].hex()
+        Block.append(byte)
+
+        if len(Block) == 16:
+            buffer.append(f"Block {Block_count}: {' '.join(Block).upper()}")
+            Block = []
+            Block_count += 1
+    return "\n".join(buffer), Block_count
+
+
+def get_uid(contents: bytes) -> str:
+    Block = []
+    for i in range(4):
+        byte = contents[i : i + 1].hex()
+        Block.append(byte)
+    return " ".join(Block).upper()
+
+
+def get_sak(contents: bytes) -> str:
+    Block = []
+    for i in range (5,6):
+        sak = contents[i : i + 1].hex()
+        Block.append(sak)
+    return " ".join(Block).upper()
+
+def get_atqa(contents: bytes) -> str:
+    Block = []
+    for i in range (6,8):
+        atqa = contents[i : i + 1].hex()
+        Block.append(atqa)
+    return " ".join(Block).upper()
+
+
+def assemble_code(contents: {hex}) -> str:
+    conversion, Block_count = convert(contents)
+
+    return f"""Filetype: Flipper NFC device
+Version: 2
+# Nfc device type can be UID, Mifare Ultralight, Mifare Classic, Bank card
+Device type: Mifare Classic
+# UID, ATQA and SAK are common for all formats
+UID: {get_uid(contents)}
+ATQA: {get_atqa(contents)}
+SAK: {get_sak(contents)}
+# Mifare Classic specific data
+Mifare Classic type: 1K
+Data format version: 2
+# Mifare Classic Blocks, '??' means unknown data
+{conversion}
+"""
+
+
+def convert_file(input_path: str, output_path: str):
+    input_extension = os.path.splitext(input_path)[1]
+    if input_extension == ".bin":
+        with open(input_path, "rb") as file:
+            contents = file.read()
+            name = os.path.split(input_path)[1]
+            write_output(name.split(".bin")[0], assemble_code(contents), output_path)
+
+
+
+def process(path: str, output_path: str):
+    if os.path.isfile(path):
+        convert_file(path, output_path)
+    else:
+        for filename in os.listdir(path):
+            new_path = os.path.join(path, filename)
+
+            if os.path.isfile(path):
+                convert_file(path, output_path)
+            else:
+                process(new_path, output_path)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input-path",
+        required=True,
+        type=pathlib.Path,
+        help="Single file or directory path to convert",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-path",
+        required=False,
+        type=pathlib.Path,
+        help="Output path, if not specified, the output .nfc file will be created in the same directory the .bin file exists within.",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    if os.path.isfile(args.input_path):
+        if not args.output_path:
+            args.output_path = os.path.split(args.input_path)[0]
+    
+    os.makedirs(args.output_path, exist_ok=True)
+    process(args.input_path, args.output_path)
+
+
+if __name__ == "__main__":
+    main()
+    print("Completed Conversion")
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ClassicConverter
-Mifare Classic Binary -> NFC file converter
+Classic Converter is a python script that converts Mifare Classic binary files into [FlipperZero's](https://flipperzero.one/) custom .nfc format. 
+  
+
+## Acknowledgements
+| [Github](https://github.com/equipter) | [Twitter](https://twitter.com/Equip0x80) | [Reddit](https://www.reddit.com/user/equipter) | equip paypal: equipter@outlook.com | [Discord](https://discord.gg/e9XzfG5nV5) |
+| :---: | :---: | :---: | :---: | :---: |
+
+ [Lucaslhm's Amiibo converter script for MFUL](https://github.com/Lucaslhm/AmiiboFlipperConverter) for code inspo
+
+## Download and Usage
+### **Requires Python**
+
+1. Download code Either through `git clone` or by simply pressing the green [Code] Button at the top and downloading the zip. 
+2. Place your (.bin) Binary files in the "assets" folder or create your own folder(s) and place the files in there.  
+## Syntax 
+The Parameters for ClassicConverter are as such 
+`-i / --input-path` - mandatory file input location, link to file in directory or whole directory to be converted. 
+
+`-o / --output-path` - optional file output location, if no output path is specified, the generated nfc file will be created in the same directory as the input binary file. 
+
+### Example
+`python3 ClassicConverter.py -i assets/example.bin`
+after running you should be met with "Completed Conversion" and a new file appearing in your assets folder with the same name as your binary file but with a .nfc extension and file format. 
+
+
+
+## Support
+
+For support, Message Equip on discord Equip#1515 or join The discord server [Link](https://discord.gg/e9XzfG5nV5)
+
